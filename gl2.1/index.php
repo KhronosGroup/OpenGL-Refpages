120295facd84c8091f7842159a3a468228aafb87
TestingPlant <44930139+TestingPlant@users.noreply.github.com>
2021-06-09 04:24:35 +0000

    Remove empty space at the top of OpenGL pages
    Fixes #92

--- gl2.1/index.php
+++ gl2.1/index.php
@@ -1,9 +1,9 @@
-<HTML>
-<HEAD>
-<TITLE>OpenGL 2.1 Reference Pages</TITLE>
-</HEAD>
-<FRAMESET ROWS="160,*">
-  <FRAME SCROLLING="no" NORESIZE FRAMEBORDER="0" MARGINWIDTH="0" MARGINHEIGHT="0" SRC="top.php">
-  <FRAME NORESIZE FRAMEBORDER="0" MARGINWIDTH="0" MARGINHEIGHT="0" SRC="bottom.php">
-</FRAMESET>
-</HTML>
+<html>
+<head>
+<title>OpenGL 2.1 Reference Pages</title>
+</head>
+<frameset cols="245,*">
+  <frame frameborder="0" marginwidth="0" marginheight="0" src="xhtml/index.html">
+  <frame name="pagedisp" frameborder="0" marginwidth="20" marginheight="20" src="xhtml/start.html">
+</frameset>
+</html>
