6cfc8a94276bbfc001fc5e058ffb8693c03f93d4
bjorn-einar-bjartnes-4ss <bjartnes@gmail.com>
2024-11-01 08:56:29 +0100

    next passthrough using php

--- es3.0/bottom.php
+++ es3.0/bottom.php
@@ -1,22 +1,9 @@
 <html>
 <head>
 <title>OpenGL ES 3.0 Reference Pages</title>
-  <script>
-    window.onload = function () {
-      try {
-        let params = new URL(document.location.toString()).searchParams;
-        let subpage = params.get("subpage");
-        if (subpage !== null) { 
-          let indexflat = document.getElementById("indexflat");
-          params.set("subpage", subpage) 
-          indexflat.src = indexflat.src + "?" + params.toString(); 
-        }
-      } catch (error) { }
-    }
-  </script>
 </head>
 <frameset cols="280,*">
-  <frame id="indexflat" frameborder="0" marginwidth="0" marginheight="0" src="html/indexflat.php">
+  <frame id="indexflat" frameborder="0" marginwidth="0" marginheight="0" src=<?php echo "html/indexflat.php?" . $_SERVER['QUERY_STRING']?> >
   <frame name="pagedisplay" frameborder="0" marginwidth="20" marginheight="20" src="html/start.html">
 </frameset>
 </html>
