18570de59e45cd80eb8a54a1814a2d5ff8590661
bjorn-einar-bjartnes-4ss <bjartnes@gmail.com>
2024-11-01 08:58:24 +0100

    And now we can get rid of the ids

--- es3.0/bottom.php
+++ es3.0/bottom.php
@@ -3,7 +3,7 @@
 <title>OpenGL ES 3.0 Reference Pages</title>
 </head>
 <frameset cols="280,*">
-  <frame id="indexflat" frameborder="0" marginwidth="0" marginheight="0" src=<?php echo "html/indexflat.php?" . $_SERVER['QUERY_STRING']?> >
+  <frame frameborder="0" marginwidth="0" marginheight="0" src=<?php echo "html/indexflat.php?" . $_SERVER['QUERY_STRING']?> >
   <frame name="pagedisplay" frameborder="0" marginwidth="20" marginheight="20" src="html/start.html">
 </frameset>
 </html>
--- es3.0/index.php
+++ es3.0/index.php
@@ -4,6 +4,6 @@
 </head>
 <frameset rows="160,*">
   <frame scrolling="no" noresize frameborder="0" marginwidth="0" marginheight="0" src="top.php">
-  <frame id="bottom" noresize frameborder="0" marginwidth="0" marginheight="0" src=<?php echo "bottom.php?" . $_SERVER['QUERY_STRING'] ?> >
+  <frame noresize frameborder="0" marginwidth="0" marginheight="0" src=<?php echo "bottom.php?" . $_SERVER['QUERY_STRING'] ?> >
 </frameset>
 </html>
