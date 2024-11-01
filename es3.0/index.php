69aea5b05c885b62991478a9f1a9e02fc6be21fd
bjorn-einar-bjartnes-4ss <bjartnes@gmail.com>
2024-11-01 08:52:57 +0100

    Passthrough query params in php to next frame

--- es3.0/index.php
+++ es3.0/index.php
@@ -1,22 +1,9 @@
 <html>
 <head>
-<title>OpenGL ES 3.0 Reference Pages</title>
-  <script>
-    window.onload = function () {
-      try {
-         let params = new URL(document.location.toString()).searchParams;
-         let subpage = params.get("subpage");
-         if (subpage !== null) { 
-           let bottomPage= document.getElementById("bottom");
-           params.set("subpage", subpage) 
-           bottomPage.src = bottomPage.src + "?" + params.toString();
-         }
-       } catch (error) { }
-     }
-  </script>
+<title>OpenGL ES 3.0 Reference Pages <?php print("hei") ?> </title>
 </head>
 <frameset rows="160,*">
   <frame scrolling="no" noresize frameborder="0" marginwidth="0" marginheight="0" src="top.php">
-  <frame id="bottom" noresize frameborder="0" marginwidth="0" marginheight="0" src="bottom.php">
+  <frame id="bottom" noresize frameborder="0" marginwidth="0" marginheight="0" src=<?php echo "bottom.php?" . $_SERVER['QUERY_STRING'] ?> >
 </frameset>
 </html>
